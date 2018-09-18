4308ae53d7eff8e65107bf719eaff7a1d457f35c
Kyle Wotring <KyleAAAEnforcement@gmail.com>
2018-09-18 16:14:19 -0400

    Initial Commit

---
+++ Kitten App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kitten App
+//
+//  Created by Kyle Wotring on 9/18/18.
+//  Copyright © 2018 Kyle Wotring. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
